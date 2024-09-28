cca1c31269b56da7391a58772ed003687015731b
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-28 18:18:39 +0700

    Kết nối Database

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';//Nhúng dotenv từ module dotenv
+dotenv.config();//Thêm config cho dotenv
+
+//Import connect database
+import connectDataBase from "./config/database";
+
 const app: Express = express();
 const port: number = 3000;
+
+
+connectDataBase();
+
 // Rest API
-app.get("/articles", (req: Request, res: Response) => {
+import Article from "./model/article.model";
+app.get("/articles", async (req: Request, res: Response) => {
+  const articles=await Article.find({
+    deleted:false
+  });
   res.json({
-    articles: []
+    articles: articles
   });
 });
 app.listen(port, () => {
